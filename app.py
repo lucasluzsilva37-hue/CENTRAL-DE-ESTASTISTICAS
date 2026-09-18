987b95927cc9f9e08fbe7abfbf9e831ee2597314
lucasluzsilva37-hue <lucasluzsilva37@gmail.com>
2026-09-18 19:52:48 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Football Stats & Probabilities", layout="wide")
+st.title("⚽ Football Match Stats & Probabilities")
+
+st.info("App initialized. Connect a sports API or load match data to begin.")
