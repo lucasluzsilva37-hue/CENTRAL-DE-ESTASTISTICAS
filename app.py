5cf9409ad263e7bd16acf7938c6ec285a3fa13cf
lucasluzsilva37-hue <lucasluzsilva37@gmail.com>
2026-09-18 22:22:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,21 @@ URL_DA_API = "https://sportap17.p.rapidapi.com/v1/sport/football/events/live"
 
 @st.cache_data(ttl=300)
 def fetch_live_data():
-    try:
-      resposta = requests.get(
- URL_DA_API,
- headers={
- "X-RapidAPI-Key": API_KEY,
- "X-RapidAPI-Host": "sportap17.p.rapidapi.com",
- },
- )
-    if resposta.status_code == 200:
-      return resposta.json()
- else:
- return None
+   try:
+        resposta = requests.get(
+            URL_DA_API,
+            headers={
+                "X-RapidAPI-Key": API_KEY,
+                "X-RapidAPI-Host": "sportap17.p.rapidapi.com",
+            },
+        )
+        if resposta.status_code == 200:
+            return resposta.json()
+        else:
+            return None
+    except Exception as e:
+        st.error(f"Error fetching data: {e}")
+        return None
  except Exception as e:
  st.error(f"Error fetching data: {e}")
  return None
